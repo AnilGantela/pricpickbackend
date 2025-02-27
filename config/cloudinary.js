eb434f87dac2cebeef53a66099de3f570a7513a3
unknown <anilkumar.gantela77@gmail.com>
2025-02-28 00:01:10 +0530

    added all products retrival of retailers with shopname

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,12 +2,6 @@ const cloudinary = require("cloudinary").v2;
 const dotEnv = require("dotenv");
 dotEnv.config();
 
-console.log("Cloudinary Config:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "HIDDEN" : "NOT SET",
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
