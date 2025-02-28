76fdadf164a906d9b3d582ab7334dec2cf0933d2
unknown <anilkumar.gantela77@gmail.com>
2025-02-28 17:49:16 +0530

    Fix dependencies for Render deployment

--- controllers/webscrap.js
+++ controllers/webscrap.js
@@ -17,7 +17,7 @@ class ProductScraper {
     try {
       this.browser = await puppeteer.launch({
         headless: "new",
-        executablePath: puppeteer.executablePath(), // ✅ Uses Puppeteer's bundled Chromium
+        executablePath: "/usr/bin/chromium-browser",
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
 
