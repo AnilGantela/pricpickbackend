73d33755189caa6a50b409ef8cc8ad7d7c6d88c4
unknown <anilkumar.gantela77@gmail.com>
2025-04-12 13:02:52 +0530

    changed app update

--- routes/userProducts.js
+++ routes/userProducts.js
@@ -6,6 +6,6 @@ router.get("/:searchName", webscrap.getProducts);
 router.post("/retailer/:searchName", webscrap.getRetailersProducts);
 router.post("/products", webscrap.getAllRetailersProducts);
 router.get("/product/:id", webscrap.getProductById);
-router.get("/retailer/:id", webscrap.getAllProductsByRetailerId);
+router.get("/retailer/:retailerId", webscrap.getAllProductsByRetailerId);
 
 module.exports = router;
